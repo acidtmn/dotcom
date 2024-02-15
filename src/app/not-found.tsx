2f983b36379b0cd1135025cf34d59caca3ff6229
acidtmn <acidtmn@yandex.ru>
2024-02-15 12:03:57 +0500

    Update not-found.tsx
    2

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -7,7 +7,7 @@ export default function NotFound() {
       <title>Страница не найдена</title>
       <div className="w-full h-[calc(50vh)] flex flex-col justify-center items-center my-32">
         <h1 className="text-4xl mb-4">404</h1>
-        <p className="mb-4 text-xl font-semibold">PСтраница не найдена</p>
+        <p className="mb-4 text-xl font-semibold">Страница не найдена</p>
         <Link href="/">
           <Button>Вернуться на домашнюю страницу</Button>
         </Link>
