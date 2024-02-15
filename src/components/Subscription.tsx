1f52de3da9dd7ac428fe35f331dbd5875e0b9a69
acidtmn <acidtmn@yandex.ru>
2024-02-15 10:00:29 +0500

    Update Subscription.tsx
    1

--- src/components/Subscription.tsx
+++ src/components/Subscription.tsx
@@ -42,14 +42,14 @@ const Subscription = ({ className }: Props) => {
     <div className={classNames("w-full bg-white shadow border rounded-2xl p-4 sm:p-8", className)}>
       <div className="mb-2 sm:mb-4 flex flex-row justify-start items-center">
         <span className="text-3xl sm:text-6xl mr-2">💌</span>
-        <span className="text-lg sm:text-2xl md:text-3xl leading-tight">Get the latest news of Memos</span>
+        <span className="text-lg sm:text-2xl md:text-3xl leading-tight">Получайте последние новости о Memos</span>
       </div>
       <form onSubmit={handleFormSubmit} className="flex flex-row justify-start items-center gap-4">
         {!subscribed && (
           <Input
             className="grow"
             type="email"
-            placeholder="Type your email..."
+            placeholder="Введите адрес электронной почты..."
             size={sm ? "lg" : "md"}
             value={email}
             onChange={(event) => handleEmailChanged(event.target.value)}
@@ -64,7 +64,7 @@ const Subscription = ({ className }: Props) => {
           disabled={!validateEmail(email)}
           onClick={handleSubscribeButtonClick}
         >
-          {subscribed ? "Subscription successful!" : "Subscribe"}
+          {subscribed ? "Подписка прошла успешно!" : "Подписаться"}
         </Button>
       </form>
     </div>
