3dcf97941ce99ed510d2769bf6fd70bb055bc44a
acidtmn <acidtmn@yandex.ru>
2024-02-15 09:47:04 +0500

    Update not-found.tsx
    1

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -4,12 +4,12 @@ import Link from "next/link";
 export default function NotFound() {
   return (
     <>
-      <title>Page Not Found</title>
+      <title>Страница не найдена</title>
       <div className="w-full h-[calc(50vh)] flex flex-col justify-center items-center my-32">
         <h1 className="text-4xl mb-4">404</h1>
-        <p className="mb-4 text-xl font-semibold">Page Not Found</p>
+        <p className="mb-4 text-xl font-semibold">PСтраница не найдена</p>
         <Link href="/">
-          <Button>Go to Home page</Button>
+          <Button>Вернуться на домашнюю страницу</Button>
         </Link>
       </div>
     </>
